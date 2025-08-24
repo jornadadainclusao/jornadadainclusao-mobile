8caf93ad2fb5910b121695553e6754ad6ff189ff
ViniMarkos283 <vinimarcos0283@gmail.com>
2025-08-24 18:02:22 -0300

    Add initial screens
    - start React Native Project using Expo
    - add tabs: games, profile and about
    - add theme auto adjust (dark and light mode)
    - add screens: login and cadastro

---
+++ integra-kids-mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,63 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; // Ícones do Ionicons
+
+import { HapticTab } from '@/components/HapticTab';
+import TabBarBackground from '@/components/ui/TabBarBackground';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarBackground: TabBarBackground,
+        tabBarStyle: Platform.select({
+          ios: {
+            position: 'absolute',
+          },
+          default: {},
+        }),
+      }}
+    >
+      {/* Jogos */}
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Jogos',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="game-controller" size={size} color={color} />
+          ),
+        }}
+      />
+
+      {/* Perfil */}
+      <Tabs.Screen
+        name="perfil"
+        options={{
+          title: 'Perfil',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" size={size} color={color} />
+          ),
+        }}
+      />
+
+      {/* Sobre */}
+      <Tabs.Screen
+        name="sobre"
+        options={{
+          title: 'Sobre',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="information-circle" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
